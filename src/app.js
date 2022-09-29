b536493a8be65424228fc7b2484c30f938ecfd0c
Jonathan Augusto Marcori <jonyls623@gmail.com>
2022-09-29 11:20:06 -0300

    no anda

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
-import  Express  from "express";
-import {pool} from "./db.js";
+import express from 'express'
+import { pool } from './db.js'
 import {PORT} from './config.js'
 
-const app = Express();
+const app = express()
 
-app.get('/', async (req, res)=> {
-  const [rows] = await pool.query('select * FROM users');
+app.get('/', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM users')
   res.json(rows)
-});
+})
 
-app.get('/ping', async (req, res)=>{
+app.get('/ping', async (req, res) => {
     const [result] = await pool.query(`SELECT "hello world" as RESULT`);
-    res.json(result[0]);
-});
+    res.json(result[0])
+})
 
 app.get('/create', async (req, res) => {
-    const result = await pool.query('INSERT INTO users(name) VALUES("jony")')
-    res.json(result);})
+    const result = await pool.query('INSERT INTO users(name) VALUES ("jony")')
+    res.json(result)
+  })
 
-app.listen(PORT);
-console.log('Server on port', PORT);//investigar modulos loggin => morgan wiston pino 
+app.listen(PORT)
+console.log('Server on port', PORT)
+//investigar modulos loggin => morgan wiston pino 
